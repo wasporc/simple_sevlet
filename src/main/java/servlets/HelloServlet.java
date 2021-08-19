24851b9309b2a2d2cf306ea28431f5b1eef345a4
wasp <wasporc@mail.ru>
2021-08-19 23:37:27 +0300

    hello servlet

---
+++ src/main/java/servlets/HelloServlet.java
@@ -0,0 +1,23 @@
+package servlets;
+
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello World");
+        printWriter.close();
+    }
+}
